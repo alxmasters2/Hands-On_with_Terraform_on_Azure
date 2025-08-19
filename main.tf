c463438cef85845f9e8e908883dca3afff8b8c53
Marlon S <lonsarmiento@gmail.com>
2025-08-19 07:08:18 +0000

    adding infrastructure

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.43.0"
+    }
+  }
+  cloud {
+    organization = "MyOrf"
+
+    workspaces {
+      name = "Hands-On_with_Terraform_on_Azure"
+    }
+  }
+
+}
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  subscription_id = "0cfe2870-d256-4119-b0a3-16293ac11bdc"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "813-fff656f2-hands-on-with-terraform-on-azure"
+  location = "southcentralus"
+}
+module "securestorage" {
+  source               = "app.terraform.io/MyOrf/securestorage/azurerm"
+  version              = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "strgzzz0000"
+}
+
